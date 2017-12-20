2cd1059f4e8a3c250c62a37a5675ed6bcc641529
Biw Walker <biwswalker@Biws-MacBook-Pro.local>
2017-12-20 16:43:20 +0700

    Biwswalker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { PageHeaderComponent } from './layout/page-header/page-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule
