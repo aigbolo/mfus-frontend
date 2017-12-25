9f507b0dba939d06844de23038a447d65621d0b3
Pariwat Viriyaphan <aigbolo@gmail.com>
2017-12-25 16:27:48 +0700

    Ig

--- src/app/officers/manage-scholarships/m030101-manage-sponsor/m030101-manage-sponsor.component.ts
+++ src/app/officers/manage-scholarships/m030101-manage-sponsor/m030101-manage-sponsor.component.ts
@@ -1,3 +1,5 @@
+import { RftProvince } from './../../../models/rft-province';
+import { Observable } from 'rxjs/Observable';
 import { ReferanceService } from './../../../services/general/reference.service';
 import { LayoutService } from './../../../services/utils/layout.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +11,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class M030101ManageSponsorComponent implements OnInit {
   image: any;
+  listProvince = new Observable<RftProvince[]>();
+  provinceList : RftProvince[] = [];
   constructor(private layoutService: LayoutService,
   private referanceService: ReferanceService) { }
 
   ngOnInit() {
     this.layoutService.setPageHeader('บันทึกผู้ให้ทุนการศึกษา');
     this.image = '../../assets/images/empty_profile.png';
+    this.listProvince = this.referanceService.getProvinces();
   }
 
+
+
+
+  // autocompleteProvince(event) {
+  //   console.log("autocompleteProvince");
+  //   let query = event.query;
+  //   this.provinceList = [];
+  //   // this.homeDistrict = new RftDistrict();
+  //   // this.homeSubDistrict = new RftSubDistrict();
+  //   let objList: RftProvince[];
+  //   objList = this.listProvince;
+  //   for (let obj of objList) {
+  //     // Filter By string event
+  //     if (obj.province_name_t.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+  //       this.homeProvinceList.push(obj);
+  //     }
+  //   }
+  // }
+
+  // handleCompleteClickProvince(index: number) {
+  //   console.log("handleCompleteClickProvince");
+  //   if (index == 0) {
+  //     this.homeProvinceList = [];
+
+  //     setTimeout(() => {
+  //       this.homeProvinceList = this.listProvince;
+  //       this.homeDistrictList = [];
+  //       this.homeSubDistrictList = [];
+  //     }, 100);
+  //   }
+  //   if (index == 1) {
+  //     this.currentProvinceList = [];
+  //     setTimeout(() => {
+  //       this.currentProvinceList = this.listProvince;
+  //       this.currentDistrictList = [];
+  //       this.currentSubDistrictList = [];
+  //     }, 100);
+  //   }
+  // }
+
+  //  // Autocomplete Selected
+  //  selectProvince(index: number) {
+  //   console.log("selectProvince");
+  //   if (index == 0) {
+  //     // this.thisForm.homeProvince = new RftProvince();
+  //     this.homeDistrict = new RftDistrict();
+  //     this.homeSubDistrict = new RftSubDistrict();
+  //     this.thisForm.acAddress.home_postcode = null;
+  //     this.utilsService
+  //       .getDistrictsByProvinceRef(this.homeProvince.province_ref)
+  //       .subscribe((res: RftDistrict[]) => {
+  //         this.homeListDistrict = [];
+  //         this.homeListDistrict.push(...res);
+  //       });
+  //   }
+  // }
 }
