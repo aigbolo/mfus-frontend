a8edd28e387712401a37b6a888cd27094c85eab7
Pariwat Viriyaphan <aigbolo@gmail.com>
2017-12-27 14:33:02 +0700

    Ig

--- src/app/services/utils/utils.service.ts
+++ src/app/services/utils/utils.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { SelectItem } from 'primeng/primeng';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 @Injectable()
@@ -8,8 +10,8 @@ export class UtilsService {
   constructor(private router: Router) { }
 
   // cri is page action such as M = manage page, S = search page
-  getActiveFlag(cri: string){
-    let activeStatus = []
+  getActiveFlag(cri: string):SelectItem[]{
+    let activeStatus:SelectItem[] = []
 
     if(cri == 'M'){
       activeStatus = [
@@ -29,7 +31,7 @@ export class UtilsService {
   }
 
 
-  getTitleList() {
+  getTitleList():SelectItem[] {
     let titleList = [
       { label: 'นาย', value: 'Mr' },
       { label: 'นาง', value: 'Miss' },
@@ -40,5 +42,34 @@ export class UtilsService {
 
   goToPage(path: string){
     this.router.navigate([path])
+  }
+
+  convertBlobToString(objectUrl:any):Observable<any>{
+    var myBlob:any = null;
+    let base64data: string = null;
+    var xhr = new XMLHttpRequest();
+    const reader = new FileReader();
+    let data = new Observable(observer => {
+      setTimeout(() => {
+        xhr.open('GET', objectUrl.changingThisBreaksApplicationSecurity, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            myBlob = xhr.response
+        };
+        xhr.send();
+
+      }, 50);
+      setTimeout(() => {
+        reader.readAsDataURL(myBlob);
+          reader.onloadend = function() {
+              base64data = reader.result;
+              observer.next(base64data);
+          }
+      }, 100);
+      setTimeout(() => {
+        observer.complete();
+      },150);
+    });
+    return data;
   }
 }
