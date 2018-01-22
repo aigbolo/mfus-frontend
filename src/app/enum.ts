1edfef2a5cae1739f330cfdbb0921e029cb57a30
unknown <jannarong.san@gmail.com>
2018-01-22 18:26:47 +0700

    Biwswalker

--- src/app/enum.ts
+++ src/app/enum.ts
@@ -1,6 +1,6 @@
 // export const URL = 'http://10.1.87.111:8000/api/'
-// export const URL = 'http://10.1.87.225:70/mfus-api/public/api/'
-export const URL = 'http://localhost/mfus-api/public/api/'
+export const URL = 'http://10.1.87.225:70/mfus-api/public/api/'
+// export const URL = 'http://localhost/mfus-api/public/api/'
 
 export const Severity = {
     SUCCESS: 'success',
