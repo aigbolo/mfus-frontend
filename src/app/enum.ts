776e69b8914fd72acce73a52f67c82df2e1e5918
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-01-26 09:58:58 +0700

    Ig

--- src/app/enum.ts
+++ src/app/enum.ts
@@ -1,5 +1,5 @@
 // export const URL = 'http://10.1.87.111:8000/api/'
-export const URL = 'http://10.1.87.225:70/mfus-api/public/api/'
+export const URL = 'http://10.1.87.225:70/mfus-api/api/'
 
 // export const URL = 'http://10.1.87.3/mfus-api/public/api/'
 
