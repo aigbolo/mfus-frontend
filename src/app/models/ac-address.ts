2322089a1497e5c5f0c627e1cec498ca3c5e3eb2
Pariwat Viriyaphan <aigbolo@gmail.com>
2018-02-06 15:18:57 +0700

    Ig

--- src/app/models/ac-address.ts
+++ src/app/models/ac-address.ts
@@ -19,4 +19,26 @@ export class AcAddress {
  public update_user: string;
  public update_datetime: Date;
 
+ constructor(){
+  this.address_ref=null;
+  this.student_ref=null;
+  this.home_address=null;
+  this.home_province=null;
+  this.home_district=null;
+  this.home_sub_district=null;
+  this.home_postcode=null;
+  this.direction_name=null;
+  this.direction_type=null;
+  this.direction_image=null;
+  this.current_address=null;
+  this.current_province=null;
+  this.current_district=null;
+  this.current_sub_district=null;
+  this.current_postcode=null;
+  this.create_user=null;
+  this.create_datetime=null;
+  this.update_user=null;
+  this.update_datetime=null;
+ }
+
 }
