a363816f468f1d323682f2e1770fc9291e3f30d1
Patikon <ultimate_last@hotmail.com>
2018-02-21 17:16:28 +0700

    knes 21/02/61

--- src/app/services/officers/m050103-scholarship-earning.service.ts
+++ src/app/services/officers/m050103-scholarship-earning.service.ts
@@ -1,13 +1,4 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
-import { ReferenceService } from './../general/reference.service';
-import { ConfigurationService } from './../utils/configuration.service';
-import { ScholarshipEarningForm } from '../../forms/scholarship-earning-form';
-
-@Injectable()
-export class M050103ScholarshipEarningService {
-
-=======
 import { ScholarshipEarningForm } from './../../forms/scholarship-earning-form';
 import { ReferenceService } from './../general/reference.service';
 import { ConfigurationService } from './../utils/configuration.service';
@@ -16,16 +7,11 @@ export class M050103ScholarshipEarningService {
 
 
 
->>>>>>> eb9f2652a84877f828f3d15f3a39823f04659633
   constructor(private config: ConfigurationService,
               private referenceService: ReferenceService,) { }
 
       doSearch(form:ScholarshipEarningForm){
         return this.config.requestMethodPOST('scholarship-grantee',form.search_criteria);
       }
-<<<<<<< HEAD
-    }
-=======
 
   }
->>>>>>> eb9f2652a84877f828f3d15f3a39823f04659633
