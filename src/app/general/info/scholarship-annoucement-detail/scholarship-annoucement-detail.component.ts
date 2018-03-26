2fbf06da8419aca96be94f81c83430cf50ad9498
unknown <jannarong.san@gmail.com>
2018-03-26 16:36:59 +0700

    biwswalker

--- src/app/general/info/scholarship-annoucement-detail/scholarship-annoucement-detail.component.ts
+++ src/app/general/info/scholarship-annoucement-detail/scholarship-annoucement-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '../../../services/utils/layout.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-scholarship-annoucement-detail',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScholarshipAnnoucementDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private layout: LayoutService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe( params => console.log(params));
+    this.layout.setPageHeader("ประกาศทุนการศึกษา" + this.activatedRoute.snapshot.queryParams);
+  }
+
 
   ngOnInit() {
   }
